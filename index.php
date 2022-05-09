87b5df3ccafe829886dd2626fb2640687b89098e
vaibhavkumartiwari-cedcommerce <vaibhavkumartiwari@cedcoss.com>
2022-05-09 15:24:11 +0530

    Write a PHP program using nested for loop that creates a chess board.

--- index.php
+++ index.php
@@ -1,14 +1,57 @@
+<style>
+    table{
+        width:400px;
+        border:1px solid black;
+        text-align:center;
+    }
+    td{
+        height:30px;
+        width:30px;
+    }
+    .black{
+        height:100%;
+        width:100%;
+        background-color:black;
+        margin:auto;
+    }
+    .white{
+        height:100%;
+        width:100%;
+        background-color:gray;
+        margin:auto;
+    }
+    
+
+
+
+</style>
+
+
+<table>
 <?php
+  for($row=0;$row<8;$row++){ ?>
+      <tr>
+      <?php
+        for($cell=0;$cell<8;$cell++){ ?>
+        <?php 
+           if(($row-$cell)%2==0) {
+        ?>
+          <td><div class="black"></div></td>
+          <?php }
+          else {
+        ?>
+           <td><div class="white"></div></td>
+          
+         
+        
 
-$num=6;
-$mul=1;
-for($i=$num;$i>1;$i--)
-    $mul*=$i;
+        <?php } } } ?>
 
-    echo($mul);
+
    
 
 
 
 
-?>
+
+</table>
